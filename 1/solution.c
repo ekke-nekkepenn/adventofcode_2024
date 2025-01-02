70bfc0e6c998d4db45bd9a658e6b6ba33cfcd0a2
Ekke Nekkepenn <denizumiekren@gmail.com>
2025-01-02 08:56:36 +0100

    solved day 1 yay!

--- 1/solution.c
+++ 1/solution.c
@@ -1,46 +1,53 @@
-// https://adventofcode.com/2024/day/1
-#include <ctype.h>
+// https://adventofcode.com/2024/day/
 #include <stdio.h> // fread() fclose() printf()
 #include <stdlib.h> // atoi() malloc() free()
-#include <string.h>
+#include <string.h> // memcpy
+
 #define FILENAME "input"
-#define IPT_SIZE 1000
+// structure of our input 1000 lines each 14 chars and 2 numbers digit size 5
+#define IPT_SIZE 1000 
 #define LINE_LEN 15
 #define DIGIT_LEN 5
 
+
 // prototypes
+void part1(FILE *file);
+void part2(FILE *file);
+void fill_lists(int left[], int right[], FILE *file);
 void bubble_sort_asc(int array[], int len);
 
-int main(void) {
+// vars
+char num[DIGIT_LEN + 1];
+char line[LINE_LEN];
 
+typedef struct node {
+    int num;
+    struct node *next;
+} node;
+  
+
+int main() {
     FILE *file = fopen(FILENAME, "r");
     if (!file) {
         return 1;
     }
+
+    num[DIGIT_LEN] = '\0'; // needed for atoi()
+    int select = 0;
+
+    select ? part1(file) : part2(file);
+}
+
+
+void part1(FILE *file) {
     int left[IPT_SIZE];
     int right[IPT_SIZE];
-
-    char num[DIGIT_LEN + 1]; // all numbers are 5 digits
-    num[DIGIT_LEN] = '\0';
-
-    char line[LINE_LEN];
-    for (int i=0; i < IPT_SIZE; i++) {
-        fgets(line, LINE_LEN, file);
-        memcpy(num, line, DIGIT_LEN);
-        left[i] = atoi(num);
-        memcpy(num, line+8, DIGIT_LEN); // +8 because right number starts at index 8
-        right[i] = atoi(num);
-    }
     
+    fill_lists(left, right, file);
 
     bubble_sort_asc(left, IPT_SIZE);
     bubble_sort_asc(right, IPT_SIZE);
 
-//    for (int i = 0; i < 50; i++) {
-//        printf("%d , %d\n", left[i], right[i]);
-//    }
-
-    // now find the distance btw elements
     int distance = 0;
     int l, r;
 
@@ -48,50 +55,62 @@ int main(void) {
         l = left[i];
         r = right[i];
 
-        if (l > r) {
-            distance += l - r;
-        }
-
-        else {
-            distance += r - l;
-        }
+        if (l > r) distance += l - r;
+        else distance += r - l;
 
     }
 
     printf("%d\n", distance);
-
-    return 0;
+    return;
 }
 
+void part2(FILE *file) {
+    int left[IPT_SIZE];
+    int right[IPT_SIZE];
 
-void bubble_sort_asc(int array[], int len) {
-    // bubble sort
-    int flag = 1;
-    int i, j, temp, switched;
-    while (flag) {
-        switched = 0;
-        i = 0;
-        j = 1;
-        for (;i < IPT_SIZE-1; i++, j++) {
-            if (array[i] > array[j]) {
-                // switches places
-                temp = array[j];
-                array[j] = array[i];
-                array[i] = temp;
+    fill_lists(left, right, file);
 
-                // mark that switch happened
-                if (switched == 0) {
-                    switched = 1;
-                }
+    int similarity_score = 0;
+    for (int i = 0; i < IPT_SIZE; ++i) {
+
+        int occurence = 0;
+        for (int j = 0; j < IPT_SIZE; ++j) {
+            if (left[i] == right[j]) {
+                ++occurence;
             }
         }
-
-        // end loop
-        if (switched == 0) {
-            flag = 0;
-        }
+        similarity_score += (occurence * left[i]); 
     }
+
+    printf("%d\n", similarity_score);
 
     return;
 }
+
+void fill_lists(int left[], int right[], FILE *file) {
+    for (int i=0; i < IPT_SIZE; i++) {
+        fgets(line, LINE_LEN, file);
+        memcpy(num, line, DIGIT_LEN);
+        left[i] = atoi(num);
+        memcpy(num, (line + DIGIT_LEN + 3), DIGIT_LEN); // the next number starts at index digit_len + 3
+        right[i] = atoi(num);
+    }
+}
+
+void bubble_sort_asc(int array[], int len) {
+    int tmp;
+
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j + i < len - 1; j++) {
+            if (array[j] > array[j+1]) {
+                tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+            }
+        }
+    }
+}
+
+
+
 
