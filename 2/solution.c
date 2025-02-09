4765b6a1f1760e219aa17cbad4e8f2173db74795
Ekke <denizumiekren@gmail.com>
2025-02-09 07:18:54 +0100

    sync

--- 2/solution.c
+++ 2/solution.c
@@ -6,10 +6,10 @@
 #include <string.h>   // memcpy
 
 #define FILENAME "input.txt"
-#define IPT_SIZE 1000
+#define IPT_SIZE 1000 // total line amount
 #define MAX_LINE_LEN 30  // technically longer than len of any line
 
-// struct
+// Struct
 typedef struct {
     char *buffer;
     int16_t len;
@@ -21,17 +21,13 @@ typedef struct {
     int16_t capacity;
 } IntArray;
 
-// prototypes
+
+// Prototypes
 IntArray convert(String line); 
-void part1();
+bool is_safe(IntArray arr);
 
-int main() {
-    part1();
 
-    return 0;
-}
-
-void part1() {
+int part1() {
     FILE *file = fopen(FILENAME, "r");
     if (!file) {
         perror("test");
@@ -40,18 +36,20 @@ void part1() {
 
     String line = (String) {malloc(sizeof(char) * MAX_LINE_LEN), 0};
 
-    int level_total = 0;
+    int safety_total = 0;
     for (int i = 0; i < IPT_SIZE; i++) {
+
+        IntArray arr;
         char c;
         for (int j = 0; j < MAX_LINE_LEN; j++) {
             fread(&c, sizeof(char), 1, file);
-            // we rached end of line
             line.len = j;
             if (c == '\n') {
                 line.buffer[j] = '\0';
                 
                 IntArray arr = convert(line);
-
+                if (is_safe(arr)) 
+                    safety_total++;
 
                 break;
             }
@@ -59,13 +57,15 @@ void part1() {
             // fill up line.buffer
             line.buffer[j] = c;
         }
+        free(arr.items);
     }
 
-    printf("%d", level_total);
+    printf("Total amount of safe lines: %d\n", safety_total);
+    return 0;
 }
 
 IntArray convert(String line) {
-    char buffer[3]; // store each num per line
+    char buffer[3]; // no number is bigger than 2 digets
 
     int capacity = 10;
     IntArray arr = (IntArray) {malloc(sizeof(int) * capacity), 0, capacity};
@@ -82,4 +82,61 @@ IntArray convert(String line) {
     }
 
     return arr;
+}
+
+bool is_safe(IntArray arr) {
+    /* this whole function is just garbage jank. I am so sorry*/
+    bool v = true;
+    int dampner = 0; // this is explained in the problem. 
+    bool inc = false; // increase in value?
+    bool dec = false; // decrease in value? 
+    bool incdec = false; // idk fk this
+
+    for (int i = 0; i < arr.len - 1; i++) {
+        int d = arr.items[i + 1] - arr.items[i];
+
+        if (0 == d || d > 3 || d < -3) {
+            v = false;
+            dampner++;
+        }
+
+        if (d > 0) inc = true; 
+        if (d < 0) dec = true;
+
+        if(inc == dec && !incdec) { // WTF AM I DOING
+            dampner++;
+            incdec = true; // this should only be once
+        }
+
+                
+    }
+    if (inc == dec) v = false;
+    if (dampner == 1) v = true;
+    return v;
+
+}
+
+//bool is_safe(IntArray arr) {
+//
+//    bool inc = false; 
+//    bool dec = false;
+//    for (int i = 0; i < arr.len - 1; i++) {
+//        int d = arr.items[i + 1] - arr.items[i];
+//        if (0 == d || d < -3 || d > 3) return false;
+//
+//        if (d > 0) inc = true;
+//        if (d < 0) dec = true;
+//        if (inc == dec) return false;
+//    }
+//
+//    return true;
+//}
+
+
+int main(void) {
+    int s = part1();
+    printf("%d\n", s);
+    
+    
+    return 0;
 }
