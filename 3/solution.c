195a4547936c468892a7455098cc71239089fb64
Ekke <denizumiekren@gmail.com>
2025-02-18 03:31:15 +0100

    sync also fk regex.h

--- 3/solution.c
+++ 3/solution.c
@@ -3,13 +3,16 @@
 #include <stdlib.h>
 
 #define FILENAME "input.txt"
-#define LINE_LEN 4000
+#define LINE_LEN 22000
 
 typedef struct {
-    char *ptr;
+    char *ptr; // beginning of string
     int length;
 } String;
 
+
+
+void print_string(String s);
 void part1(void);
 void free_string(String s);
 
@@ -25,13 +28,55 @@ void part1(void) {
         printf("Could not open %s\n", FILENAME);
         return;
     }
-    String Line = (String) {malloc(sizeof(char) * LINE_LEN), 0};
+    String Text = (String) {malloc(sizeof(char) * LINE_LEN), 0};
 
-    free_string(Line);
+    char c;
+    while (fread(&c, sizeof(char), 1, file)) {
+        Text.ptr[Text.length] = c;
+        Text.length++;
+    }
+    
+    print_string(Text);
+    printf("\n\n\n");
+
+    regex_t regex;
+    while (1) {
+        char msgbuf[100];
+        char *pattern = "mul\\(+\\d,+\\d\\)";
+        int regc = regcomp(&regex, pattern, 0);
+        if (regc) {
+            break;
+        }
+
+        regc = regexec(&regex, Text.ptr, 0, NULL, 0);
+        if (!regc) {
+            printf("Match");
+        }
+        else if (regc == REG_NOMATCH) {
+            printf("No Match");
+        }
+        else {
+            regerror(regc, &regex, msgbuf, sizeof(msgbuf));
+            break;
+        }
+        break;
+    }
+    for (int i = 0; i < Text.length; i++)
+
+
+    regfree(&regex);
+    free_string(Text);
+
     fclose(file);
 }
 
+void print_string(String s) {
+    for (int i = 0; i < s.length; i++) {
+        printf("%c", s.ptr[i]);
+    }
+}
 
 void free_string(String s) {
+    //printf("(free_string) address ptr: %p\n\n", s.ptr);
     free(s.ptr);
 }
