aa92858534bb0c1abb8d0d1d3356fe6f51c69f2e
Ekke <denizumiekren@gmail.com>
2025-03-01 15:16:25 +0100

    solved day 3 with a little bit of help by reddit user ZombiFeynman

--- 3/solution.c
+++ 3/solution.c
@@ -6,7 +6,6 @@
 #include <string.h> // memcpy()
 
 #define FILENAME "input.txt"
-#define LINE_LEN 22000 // used for part1 not needed for part2
 #define MAX_DIGIT 10
 
 #define PATTERN_MUL "mul\\(([0-9]+),([0-9]+)\\)"
@@ -22,7 +21,7 @@ typedef struct {
 void part1(void);
 void part2(void);
 
-int64_t find_muls(char* s, int64_t* oS, int64_t* oE, regex_t* regex_mul, regmatch_t* match_mul);
+int64_t find_muls(char* string, int64_t offset_start, int64_t offset_end, regex_t* regex_mul, regmatch_t* match_mul);
 
 void free_string(String s);
 void print_string(String s);
@@ -43,6 +42,7 @@ void part1(void)
         return;
     }
 
+    // determine length of file
     fseek(file, 0, SEEK_END); // go to the end pos of file
     int64_t file_length = ftell(file); // returns current pos which is the length
     fseek(file, 0, SEEK_SET);
@@ -106,7 +106,7 @@ void part2(void)
     // open file
     FILE* file = fopen(FILENAME, "r");
     if (file == NULL) {
-        printf("File could not opened.");
+        printf("File could not be opened.");
         return;
     }
 
@@ -127,7 +127,7 @@ void part2(void)
     regex_t regex_do;
 
     if (regcomp(&regex_mul, PATTERN_MUL, REG_EXTENDED) || regcomp(&regex_dont, PATTERN_DONT, REG_EXTENDED)
-        || regcomp(&regex_do, PATTERN_DONT, REG_EXTENDED)) {
+        || regcomp(&regex_do, PATTERN_DO, REG_EXTENDED)) {
         printf("Regex compiliation failed.\n");
         free_string(Text);
         return;
@@ -137,17 +137,34 @@ void part2(void)
     regmatch_t match_dont[1];
     regmatch_t match_do[1];
 
-    int64_t offset_start = 0;
-    int64_t offset_end = 0;
-
-    bool donts = true;
+    int64_t s_start = 0; // slice start
+    int64_t s_end = 0; // slice end
 
     int64_t total = 0;
-    while (true) {
+    while (s_end < Text.length) {
+        if (regexec(&regex_dont, Text.ptr + s_start, 1, match_dont, 0) != 0) {
+            // failed to find a dont()
+            s_end = Text.length;
+        } else {
+            // if regexec was successful
+            s_end = s_start + match_dont[0].rm_eo;
+        }
+
+        // string "slice" s_start to s_end
+        char tmp = Text.ptr[s_end];
+        Text.ptr[s_end] = '\0'; // insert so regexec knows at s_end to stop
+        total = total + find_muls(Text.ptr, s_start, s_end, &regex_mul, match_mul);
+        Text.ptr[s_end] = tmp;
+
+        // now we find update start with offset end of next do()
+        if (regexec(&regex_do, Text.ptr + s_end, 1, match_do, 0) != 0) {
+            // no do() found so all remaining mul() invalid
+            break;
+        }
+        s_start = s_end + match_do[0].rm_eo;
     }
 
-    char tmp;
-    char num_buffer[MAX_DIGIT];
+    printf("%ld\n", total);
 
     // free things
     free_string(Text);
@@ -156,23 +173,32 @@ void part2(void)
     regfree(&regex_do);
 }
 
-int64_t find_muls(char* s, int64_t* offset_start, int64_t* offset_end, regex_t* regex_mul, regmatch_t* match_mul)
+int64_t find_muls(char* string, int64_t offset_start, int64_t offset_end, regex_t* regex_mul, regmatch_t* match_mul)
 {
-    // modify s so it ends at *oE with a '\0' so regexec() knows where to stop
-    char tmp = s[*offset_end];
-    s[*offset_end] = '\0';
+    char buffer[MAX_DIGIT];
 
-    int64_t num = 0;
-
-    while (true) {
-        if (regexec(regex_mul, s + *offset_start, 3, match_mul, 0) != 0) {
+    int64_t total = 0;
+    while (offset_start < offset_end) {
+        if (regexec(regex_mul, string + offset_start, 3, match_mul, 0) != 0) {
             break;
         }
 
-        *offset_start = *offset_start + match_mul[0].rm_eo;
+        int s;
+        int e;
+        int num = 1;
+        // gets the 2 subexpressions in match_mul[i] and multiplies them both
+        for (int i = 1; i < 3; ++i) {
+            s = match_mul[i].rm_so;
+            e = match_mul[i].rm_eo;
+            memcpy(buffer, string + offset_start + s, e - s);
+            buffer[e - s] = '\0'; // if previous write to buffer is still there
+            num = num * atoi(buffer);
+        }
+        total += num;
+        offset_start += e; // advance start pos of slice
     }
-    s[*offset_end] = tmp;
-    return num;
+
+    return total;
 }
 
 void print_string(String s)
