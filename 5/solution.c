70ead84f883e19cad9d31262f2f7ce7f3fd85889
Ekke <denizumiekren@gmail[1;5D.com>
2025-03-27 03:35:54 +0100

    added Puzzle 5

---
+++ 5/solution.c
@@ -0,0 +1,12 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    // create a directed graph with numA -> [numB, numC, ...]
+    // numA has higher priority than nums that are pointed to by it
+    // how do we create a directed graph?
+
+    return 0;
+}
